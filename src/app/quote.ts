8d339ef0485b3fa5cd7aae50998ba3a212943b0a
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-04-25 21:32:04 +0300

    added code to my ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,8 @@
 export class Quote {
-  constructor(public id:number, public name:string, public author:string, public submit:string, public upvotes:number, public downvotes:number, public quoteDate:Date){}
+  public showAuthor:boolean;
+  public showSubmit:boolean;
+  constructor(public id:number, public name:string, public author:string, public submit:string, public upvotes:number, public downvotes:number, public quoteDate:Date){
+  this.showAuthor=false;
+  this.showSubmit=false;
+}
 }
